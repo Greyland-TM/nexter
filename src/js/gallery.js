90854024a60d9fbebf33f7c8f505637298d5dc8e
Greyland-TM <greyland.official@gmail.com>
2021-01-16 20:49:56 -0800

    goodnight

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import gal1 from '../madia/gal-1.jpeg';
+// import gal2 from '../madia/gal-2.jpeg';
+// import gal3 from '../madia/gal-3.jpeg';
+// import gal4 from '../madia/gal-4.jpeg';
+// import gal5 from '../madia/gal-5.jpeg';
+// import gal6 from '../madia/gal-6.jpeg';
+// import gal7 from '../madia/gal-7.jpeg';
+// import gal8 from '../madia/gal-8.jpeg';
+// import gal9 from '../madia/gal-9.jpeg';
 
 const Gallery = () => (
     <section className="gallery">
-        Gallery
+        <img src={gal1} alt="Galleryimage 1" className="gallery__img"/>
     </section>
 );
 
